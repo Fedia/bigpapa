9c684906d819ff0e82f340b0ddc05fac3806d52c
Fedor <fedia@psih.ru>
2018-08-01 00:28:52 +0300

    json schemas

--- ingest/index.js
+++ ingest/index.js
@@ -11,8 +11,9 @@ exports.ingestLogs = (data, context) => {
   const file = data;
   if (!file.name.match(/_usage_/)) return true;
   const datasetName = file.bucket.replace('-logs', '');
-  return BigQuery.query({
-    query: `
+  return BigQuery.dataset(datasetName)
+    .query({
+      query: `
     CREATE TEMP FUNCTION URLDECODE(url STRING) AS ((
       SELECT STRING_AGG(
         IF(REGEXP_CONTAINS(y, r'^%[0-9a-fA-F]{2}'), 
@@ -22,42 +23,31 @@ exports.ingestLogs = (data, context) => {
       FROM UNNEST(REGEXP_EXTRACT_ALL(url, r"%[0-9a-fA-F]{2}(?:%[0-9a-fA-F]{2})*|[^%]+")) y
       WITH OFFSET AS i 
     ));
-    SELECT
-      DATE(TIMESTAMP_MICROS(CAST(time_micros AS INT64))) dt,
-      s_request_id id, c_ip ip, cs_host host, cs_object beacon, cs_referer ref, cs_user_agent ua,
-      TIMESTAMP_MILLIS(CAST(JSON_EXTRACT_SCALAR(data, "$.t") AS INT64)) ts,
-      JSON_EXTRACT_SCALAR(data, "$.u") uid, JSON_EXTRACT_SCALAR(data, "$.e") event,
-      JSON_EXTRACT_SCALAR(data, "$.l") href, JSON_EXTRACT_SCALAR(data, "$.s") sel,
-      data
+    INSERT INTO ${datasetName}.logs (dt, id, ip, host, beacon, ref, ua, ts, uid, event, href, sel, data) SELECT
+      DATE(TIMESTAMP_MICROS(CAST(time_micros AS INT64))),
+      s_request_id, c_ip, cs_host, cs_object, cs_referer, cs_user_agent,
+      TIMESTAMP_MILLIS(CAST(JSON_EXTRACT_SCALAR(data, "$.t") AS INT64)),
+      JSON_EXTRACT_SCALAR(data, "$.u"), JSON_EXTRACT_SCALAR(data, "$.e"),
+      JSON_EXTRACT_SCALAR(data, "$.l"), JSON_EXTRACT_SCALAR(data, "$.s"), data
       FROM (
-        SELECT *, URLDECODE(SUBSTR(cs_uri, STRPOS(cs_uri, "?")+1)) data FROM '${datasetName}'.logsbucket
+        SELECT *, URLDECODE(SUBSTR(cs_uri, STRPOS(cs_uri, "?")+1)) data FROM logsbucket
         WHERE cs_method="GET" AND sc_status=200 AND ENDS_WITH(cs_object, ".gif")
       );
     `,
-    tableDefinitions: {
-      logsbucket: {
-        sourceFormat: 'CSV',
-        csvOptions: {
-          skipLeadingRows: 1
-        },
-        sourceUris: [`gs://${file.bucket}/${file.name}`],
-        schema: {
-          fields: logSchema
+      tableDefinitions: {
+        logsbucket: {
+          sourceFormat: 'CSV',
+          csvOptions: {
+            skipLeadingRows: 1
+          },
+          sourceUris: [`gs://${file.bucket}/${file.name}`],
+          schema: {
+            fields: logSchema
+          }
         }
-      }
-    },
-    destinationTable: {
-      datasetId: datasetName,
-      tableId: 'logs'
-    },
-    createDisposition: 'CREATE_IF_NEEDED',
-    writeDisposition: 'WRITE_APPEND',
-    timePartitioning: {
-      type: 'DAY',
-      field: 'dt'
-    },
-    useLegacySql: false
-  })
+      },
+      useLegacySql: false
+    })
     .then(res => {
       console.log('Ingested', file.name);
     })
--- ingest/setup.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE logs (dt DATE, id STRING, ip STRING, host STRING, beacon STRING, ref STRING, ua STRING, ts TIMESTAMP, uid STRING, event STRING, href STRING, sel STRING, data STRING)
-PARTITION BY dt;
